0d19d6be11c182c12bd9095377ef6f0b1a458e8e
kevin <kevin@kevindeMacBook-Pro.local>
2022-08-12 21:49:07 +0800

    Update.

--- src/io/kevin197011/cicd/Gitlab.groovy
+++ src/io/kevin197011/cicd/Gitlab.groovy
@@ -9,7 +9,7 @@ class Gitlab {
         script.echo("git clone chatOps!")
 
         def appsDir = new File('/tmp/apps')
-        if (!iappsDir.exists()){
+        if (!appsDir.exists()){
             appsDir.mkdirs()
         }
 //        script.sh('[[ -d /tmp/apps || mkdir /tmp/apps ]]')
