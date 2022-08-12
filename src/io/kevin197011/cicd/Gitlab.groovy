1038e53564651d8534d782b3d5b2408ba0c4dea8
kevin <kevin@kevindeMacBook-Pro.local>
2022-08-12 21:48:32 +0800

    Update.

--- src/io/kevin197011/cicd/Gitlab.groovy
+++ src/io/kevin197011/cicd/Gitlab.groovy
@@ -7,7 +7,12 @@ class Gitlab {
 
     void gitCloneItem(String repo, String token, String branch = 'master') {
         script.echo("git clone chatOps!")
-        script.sh('[[ -d /tmp/apps || mkdir /tmp/apps ]]')
+
+        def appsDir = new File('/tmp/apps')
+        if (!iappsDir.exists()){
+            appsDir.mkdirs()
+        }
+//        script.sh('[[ -d /tmp/apps || mkdir /tmp/apps ]]')
         script.sh("git clone https://github.com/kevin197011/chatOps.git /tmp/apps/chatOps")
 //        script.git(url: "https://github.com/kevin197011/chatOps.git", branch: 'master')
     }
