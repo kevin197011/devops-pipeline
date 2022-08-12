21e64a2902cdc3cbe25ddfe25a43ac4a8eee48d4
kevin <kevin@kevindeMacBook-Pro.local>
2022-08-12 18:21:21 +0800

    Update.

--- vars/tt.groovy
+++ vars/tt.groovy
@@ -5,11 +5,13 @@ import io.kevin197011.cicd.Database
 def call() {
 
     def sqlData = '''
-    CREATE TABLE Author (
-    id          INTEGER GENERATED,
-    firstname   VARCHAR(64),
-    lastname    VARCHAR(64)
-  );
+CREATE TABLE IF NOT EXISTS `runoob_tbl`(
+   `runoob_id` INT UNSIGNED AUTO_INCREMENT,
+   `runoob_title` VARCHAR(100) NOT NULL,
+   `runoob_author` VARCHAR(40) NOT NULL,
+   `submission_date` DATE,
+   PRIMARY KEY ( `runoob_id` )
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
 '''
 
     def database = new Database('localhost', 'devops', '123456', sqlData)
