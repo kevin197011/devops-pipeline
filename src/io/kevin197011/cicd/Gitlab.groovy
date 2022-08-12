9b1be2a14e0bb5243ca3d33736d749ef53b27851
kevin <kevin@kevindeMacBook-Pro.local>
2022-08-12 20:08:45 +0800

    Update.

--- src/io/kevin197011/cicd/Gitlab.groovy
+++ src/io/kevin197011/cicd/Gitlab.groovy
@@ -1,12 +1,12 @@
 package io.kevin197011.cicd
 
-class Gitlab implements Serializable {
+class Gitlab {
 
-    static boolean gitCloneItem(String repo, String token, String branch = 'master') {
+    static boolean gitCloneItem(script, String repo, String token, String branch = 'master') {
 //        git(url: repo,
 //                credentialsId: token,
 //                branch: branch)
 
-        pipeline.sh("git clone ${repo}")
+        script.sh("git clone ${repo}")
     }
 }
