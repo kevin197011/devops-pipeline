af51fc619d30c2c2719dfc440436db8e172ee968
kevin <kevin@kevindeMacBook-Pro.local>
2022-08-12 19:43:41 +0800

    Update.

--- src/io/kevin197011/cicd/Gitlab.groovy
+++ src/io/kevin197011/cicd/Gitlab.groovy
@@ -3,9 +3,16 @@ package io.kevin197011.cicd
 class Gitlab {
 
     static boolean gitCloneItem(String repo, String token, String branch = 'master') {
-        git(url: repo,
-                credentialsId: token,
-                branch: branch)
+//        git(url: repo,
+//                credentialsId: token,
+//                branch: branch)
+
+        deleteDir()
+        checkout([$class           : 'GitSCM',
+                  branches         : [[name: "*/${branch}"]],
+                  userRemoteConfigs: [[credentialsId: token,
+                                       url          : repo]]])
+
     }
 
 }
