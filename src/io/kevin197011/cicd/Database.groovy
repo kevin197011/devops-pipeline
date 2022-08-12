7194a832f6a97f9ffa69946954f5c3e8ee82c54c
kevin <kevin@kevindeMacBook-Pro.local>
2022-08-12 17:34:01 +0800

    Update.

---
+++ src/io/kevin197011/cicd/Database.groovy
@@ -0,0 +1,25 @@
+package io.kevin197011.cicd
+
+import groovy.sql.Sql
+
+class Database {
+    private String host
+    private String username
+    private String password
+    private String sqlData
+
+    Database(String host, String username, String password, String sqlData) {
+        this.host = host
+        this.username = username
+        this.password = password
+        this.sqlData = sqlData
+    }
+
+    boolean execute() {
+        Sql instance = Sql.newInstance("jdbc:mysql://" + this.host + "?allowMultiQueries=true", this.username, this.password, "com.mysql.jdbc.Driver")
+        boolean execute = instance.execute(sqlDate)
+        instance.close()
+        return execute
+    }
+
+}
