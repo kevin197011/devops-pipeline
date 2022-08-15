ba9ef3ca081a252b752fae981a1f8dae4d5523b2
kevin <kevin@kevindeMacBook-Pro.local>
2022-08-15 11:01:46 +0800

    Update.

--- vars/tt.groovy
+++ vars/tt.groovy
@@ -34,26 +34,6 @@ def call() {
 //        }
 
 
-        parameters {
-            activeChoiceParam('choice1') {
-                description('select your choice')
-                choiceType('RADIO')
-                groovyScript {
-                    script("return['aaa','bbb']")
-                    fallbackScript('return ["error"]')
-                }
-            }
-            activeChoiceReactiveParam('choice2') {
-                description('select your choice')
-                choiceType('RADIO')
-                groovyScript {
-                    script("if(choice1.equals('aaa')){return ['a', 'b']} else {return ['aaaaaa','fffffff']}")
-                    fallbackScript('return ["error"]')
-                }
-                referencedParameter('choice1')
-            }
-        }
-
         stages {
 
             stage('Parameters') {
